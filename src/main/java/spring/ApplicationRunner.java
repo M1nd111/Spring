79cbafb42695cfbcee29344879df2ac379dd934c
Mind <you@example.com>
2024-05-15 01:59:56 +1000

    controller hello and bye

--- src/main/java/spring/ApplicationRunner.java
+++ src/main/java/spring/ApplicationRunner.java
@@ -2,17 +2,13 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.core.SpringProperties;
-import spring.config.DatabaseProperties;
+
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ApplicationRunner {
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
-        System.out.println(SpringProperties.getProperty("test.msg"));
-        System.out.println(context.getBean(DatabaseProperties.class));
     }
 }
