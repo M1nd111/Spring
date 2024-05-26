f69faa6b050dc834bede1d6931bea08b61a6b99a
Mind <you@example.com>
2024-05-26 18:24:36 +1000

    spring security finish - next commit for AOP

--- src/main/java/spring/service/UserService.java
+++ src/main/java/spring/service/UserService.java
@@ -2,15 +2,12 @@ package spring.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.dataBase.repository.UserRepository;
-import spring.mapper.UserMapper;
 
 @RequiredArgsConstructor
 @ToString
 @Service
 public class UserService {
-    private final UserMapper userMapper;
     private final UserRepository userRepository;
 }
